6efe719d236b94e373ac59888e0851883dc26807
Figaro4545 <wasumin454@gmail.com>
2021-07-27 18:20:01 +0300

    Todo

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
 const styles = {
@@ -11,7 +12,7 @@ const styles = {
 
 }
 
-export default function TodoList(props) {
+function TodoList(props) {
     return (
         <ul style={styles.ul}>
             {props.todos.map((todo, index) => {
@@ -21,3 +22,9 @@ export default function TodoList(props) {
         </ul>
     )
 }
+
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(PropTypes.object).isRequired
+}
+
+export default TodoList
